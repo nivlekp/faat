c77b8c112b6a62f9dc7a57acc8105d78fad4bf03
nivlekp <osamupang@gmail.com>
2022-09-14 16:49:58 +1000

    add placeholder doctest

--- faat/library.py
+++ faat/library.py
@@ -2,6 +2,11 @@ import abjad
 
 
 def make_empty_score():
+    """
+    >>> from faat import library
+    >>> library.make_empty_score()
+    Score('{ { } }', name='Score', simultaneous=True)
+    """
     piano_music_voice = abjad.Voice(name="Piano.Music")
     piano_music_staff = abjad.Staff([piano_music_voice], name="Piano.Staff")
     score = abjad.Score([piano_music_staff], name="Score")
